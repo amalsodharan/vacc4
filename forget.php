b6f1a10e1b91b91153962654610b2363333c5faa
amalsodharan <138609327+amalsodharan@users.noreply.github.com>
2023-10-13 12:55:52 +0530

    Add files via upload

---
+++ forget.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forget password</title>
+    <link rel="stylesheet" href="forget.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"> </script>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+</head>
+<body>
+        <header>
+            <nav>
+            <div class="top">
+                <div class="row">
+                    <div class="col">
+                        <h2>e-vaccines</h2>
+                    </div>
+                    <div class="col-5">
+                        <ul>
+                            <li><a href="index.php">HOME</a></li>
+                            <li><a href="login.php">LOGIN</a></li>
+                            <li><a href="#">ABOUT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </nav>
+        </header><br><br><br><br>
+        <h2>Forget password</h2><br><br>
+    <div class="cont"><br><br>
+        <input type="text" placeholder="Enter your username" class="bar" name="username"><br><br>
+        <input type="password" placeholder="Enter your password" class="bar" name="password"><br><br>
+        <input type="password" placeholder="Confirm your password" class="bar" name="password"><br><br>
+        <input type="submit" value="Create new password" class="but">
+    </div>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"> </script>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="stylesheet" href="search.css">
+    <link rel="stylesheet" href="search2.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="top">
+                <div class="row">
+                    <div class="col">
+                        <h2>e-vaccines</h2>
+                    </div>
+                    <div class="col-5">
+                        <ul>
+                            <li><a href="index.php">HOME</a></li>
+                            <li><a href="login.php">LOGIN</a></li>
+                            <li><a href="#">ABOUT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header><br><br><br><br><br>
+    <form action="">
+    <div class="vacc">
+        <h2 style="text-align: center;color: rgba(21, 98, 212, 0.89);font-size: 35px;">Search your slot</h2>
+        <center>
+        <div class="age">
+            <hr>
+            <div class="age-head">
+                <h4>Age</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-outlined">12-17</label>
+                <input type="checkbox" class="btn-check" id="btn-check-2-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-2-outlined">18+</label>
+                <input type="checkbox" class="btn-check" id="btn-check-3-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-3-outlined">45+</label>
+            </div>
+        </div>
+        <div class="age">
+            <hr>
+            <div class="age-head">
+                <h4>Dose</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="btn-check-4-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-4-outlined">Dose 1</label>
+                <input type="checkbox" class="btn-check" id="btn-check-5-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-5-outlined">Dose 2</label>
+            </div>
+        </div>
+        <div class="age">
+            <hr>
+            <div class="age-head">
+                <h4>Cost</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="btn-check-6-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-6-outlined">Free</label>
+            </div>
+        </div>
+        <div class="age">
+            <hr>
+            <div class="age-head">
+                <h4>Vaccine</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="btn-check-7-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-7-outlined">Covishield</label>
+                <input type="checkbox" class="btn-check" id="btn-check-8-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-8-outlined">Covaxin</label>
+            </div>
+        </div>
+        </center><br><br> 
+        <div class="overflow-auto" id="dat">
+        <div class="datee">
+            <ul>
+                <li>
+                    <input type="checkbox" class="btn-check" id="btn-check-9-outlined" autocomplete="off">
+                    <label class="btn btn-outline-primary btn-sm" for="btn-check-9-outlined">Mon,1 jan</label>
+                </li>
+                <li><a href="#"><button class="dat-but">Tue,2 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,3 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,4 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,5 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,6 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,7 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,8 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,9 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,10 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,11 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,12 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,13 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,14 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,15 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,16 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,17 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,18 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,19 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,20 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,21 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,22 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,23 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,24 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,25 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,26 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,27 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,28 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,29 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,30 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,31 jan</button></a></li>
+            </ul>
+        </div>
+        </div><br><br>
+        <div class="subm">
+            <input type="submit" value="Check for Avialability" class="but"><br><br>
+        </div>
+    </form>
+        <div class="last">
+            <h5>For Grievances and Feedback :<br> evaccine@gmail.com<br><br>
+            Ph no : 0481-246861<br><br>
+            Privacy Policy | Terms of Service<br><br>
+            Copyright © 2021 e-vaccine. All Rights Reserved
+            </h5> 
+        </div>
+</body>
+</html>
